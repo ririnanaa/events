d01ff9454e760c964bbe249a88eb9f24ee8240fd
rinaS <158763752+ririnanaa@users.noreply.github.com>
2024-07-14 07:59:12 +0000

    make model/ticket

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
+  delete "/logout" => "sessions#destroy", as: :logout
+  
+  resources :events do
+    resources :tickets
+  end
 end
